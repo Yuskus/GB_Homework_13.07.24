f0c5a457fc79f38495ad7243ca8bb448cb8acc94
Yuskus <yuskus@yandex.ru>
2024-07-13 11:56:27 +0300

    Добавьте файлы проекта.

---
+++ HomeworkGB8/Person.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace HomeworkGB8
+{
+    [XmlRoot]
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly Birthday { get; set; }
+        public List<string>? Hobbies { get; set; }
+        public List<Person>? Children { get; set; }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Имя: {FirstName}");
+            sb.Append(", ");
+            sb.Append($"Фамилия: {LastName}");
+            sb.Append(", ");
+            sb.Append($"День рождения: {Birthday}");
+            sb.Append(", ");
+            sb.Append("[ Хобби: " + string.Join(", ", Hobbies ?? []) + " ]");
+            sb.Append(", ");
+            sb.Append("[ Дети: " + Children?.Count + " ]");
+            sb.Append(". ");
+            return sb.ToString();
+        }
+    }
+}
---
+++ HomeworkGB8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Serialization;
+using System.Text.Json;
+
+namespace HomeworkGB8
+{
+    internal class Program
+    {
+        const string jsonExample = """
+            {
+              "FirstName": "Julia",
+              "LastName": "Vinogradova",
+              "Birthday": "1995-01-22",
+              "Hobbies": [
+                "Painting",
+                "Music",
+                "Reading",
+                "Brewing",
+                "CSharp"
+              ],
+              "Children": [
+                {
+                  "FirstName": "Maxim",
+                  "LastName": "Erashov",
+                  "Birthday": "2021-04-07",
+                  "Hobbies": [
+                    "Music",
+                    "Whims",
+                    "Eating delicious",
+                    "Playing phone games"
+                  ],
+                  "Children": []
+                }
+              ]
+            }
+            """;
+        static void Main()
+        {
+            Console.WriteLine("Так выгдядит наш json: \n");
+            Console.WriteLine(jsonExample + "\n");
+
+            Console.WriteLine("Это экземпляр класса, десериализованный из json: \n");
+            Person? someJsonPerson = JsonSerializer.Deserialize<Person>(jsonExample);
+            Console.WriteLine(someJsonPerson + "\n");
+
+            Console.WriteLine("Конвертируем json в xml: \n");
+            new XmlSerializer(typeof(Person)).Serialize(Console.Out, someJsonPerson);
+
+            Console.ReadKey(true);
+        }
+    }
+}
